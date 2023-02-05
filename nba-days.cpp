d6e682bcf6404afa0b1358b23ca94ec8666933b0
Gabriel Accetta <gmta1410@gmail.com>
2023-02-05 19:14:11 -0300

    Add files via upload

--- nba-days.cpp
+++ nba-days.cpp
@@ -1,16 +1,27 @@
 #include <windows.h>
 #include <string.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
+    // Hide console
+    ::ShowWindow(::GetConsoleWindow(), SW_HIDE);
+
+    // Create current_directory and filename
     wchar_t current_directory[MAX_PATH];
     wchar_t filename[] = L"\\days\\00.jpg";
 
     // Get current working directory
     GetCurrentDirectoryW(MAX_PATH, current_directory);
 
-    // Change filename to current day
-    filename[6] = __DATE__[4];
-    filename[7] = __DATE__[5];
+    // Get current day
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+
+    // Assign current day to filename
+    filename[6] = (int)(tm.tm_mday/10) + 48;
+    filename[7] = (int)(tm.tm_mday%10) + 48;
 
     // Concatenate current_directory and filename
     wcscat(current_directory, filename);
